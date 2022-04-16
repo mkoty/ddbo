8a999e15f49d85dc134db01f20c88a8a42b70d6d
mkoty <mkoty@yandex.ru>
2022-04-17 02:09:23 +0300

    init tabs and picture placeholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ddbo';
+  activeLink = '';
+
+  constructor(private router: Router) {
+    this.activeLink = router.url.replace('/', '');
+    // debugger;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTabsModule } from "@angular/material/tabs";
+import { YouComponent } from './you/you.component';
+import { AboutComponent } from "./about/about.component";
+import { FireComponent } from "./fire/fire.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YouComponent,
+    AboutComponent,
+    FireComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
